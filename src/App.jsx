a6ebd370ef00b2a2fd4234862f71ea2cfa0b4f48
dhiraj002 <dhirajdhiman15@gmail.com>
2024-04-10 00:50:11 +0530

    editied backgroung Img

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/164527/pexels-photo-164527.jpeg?auto=compress&cs=tinysrgb&w=400')`,
+        backgroundImage: `url('https://images.pexels.com/photos/8369648/pexels-photo-8369648.jpeg?auto=compress&cs=tinysrgb&w=600')`,
       }}
     >
       <div className="w-full">
